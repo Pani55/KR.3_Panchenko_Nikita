0a6e8f63949a62ea17b9b5d781ba49b954bbc07f
Pani_55 <Dumbasss_rus@mail.ru>
2024-02-29 22:09:51 +0300

    add comments to main

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import src.utils
 
 
-temp = src.utils.open_file__extract_info()
-temp = src.utils.reformate_date(temp)
-temp = src.utils.sort_by_date_time(temp)
-temp = src.utils.hide_from(temp)
-temp = src.utils.hide_to(temp)
-temp = src.utils.reverse_date(temp)
+temp = src.utils.open_file__extract_info()    # Присваиваю переменной temp данные из файла json
 
 
-src.utils.print_result(temp)
+temp = src.utils.reformate_date(temp)   # Форматирую дату и время для дальнейшей сортировки
+temp = src.utils.sort_by_date_time(temp)    # Сортирую по дате и времени в обратном порядке
+temp = src.utils.hide_from(temp)    # Маскирую отправителя
+temp = src.utils.hide_to(temp)    # Маскирую получателя
+temp = src.utils.reverse_date(temp)    # Переварачиваю дату на привычный лад
+
+
+src.utils.print_result(temp)    # Вывожу на экран результат
